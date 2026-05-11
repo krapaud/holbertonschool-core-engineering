bd06c93bc3908f9b5cdb03ac38600b2e1add1e3f
krapaud <mur.mickael@gmail.com>
2026-05-11 16:17:44 +0200

    Fix typo in position setter error message for clarity

--- python_oop/classes_and_object_model/6-square.py
+++ python_oop/classes_and_object_model/6-square.py
@@ -34,7 +34,7 @@ class Square:
         """Setter for position: must be a tuple of 2 non-negative integers."""
         if (not isinstance(value, tuple) or len(value) != 2
                 or not all(isinstance(n, int) and n >= 0 for n in value)):
-            raise TypeError("position must be a tuple of 2 positive integer")
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
