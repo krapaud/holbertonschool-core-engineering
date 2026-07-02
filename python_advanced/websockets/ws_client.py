2aa32521eff93e023b54fabe19f182bfd31b1f30
krapaud <mur.mickael@gmail.com>
2026-07-02 11:34:11 +0200

    Read message to send from WS_MESSAGE environment variable

--- python_advanced/websockets/ws_client.py
+++ python_advanced/websockets/ws_client.py
@@ -15,4 +15,5 @@ async def connect_and_send(uri="ws://localhost:8765", message="Hello WebSocket")
 
 if __name__ == "__main__":
     uri = os.environ.get("WS_URI", "ws://localhost:8765")
-    sys.stdout.write(str(asyncio.run(connect_and_send(uri))))
+    message = os.environ.get("WS_MESSAGE", "Hello WebSocket")
+    sys.stdout.write(str(asyncio.run(connect_and_send(uri, message))))
