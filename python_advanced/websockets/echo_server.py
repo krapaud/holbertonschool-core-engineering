147aa1a8b4c80c56a0bb557ba7a998798c2d7dba
krapaud <mur.mickael@gmail.com>
2026-07-02 11:29:58 +0200

    Rename echo handler to connection_handler for correction compatibility

--- python_advanced/websockets/echo_server.py
+++ python_advanced/websockets/echo_server.py
@@ -4,13 +4,13 @@ import asyncio
 import websockets
 
 
-async def echo(websocket):
+async def connection_handler(websocket):
     async for message in websocket:
         await websocket.send(message)
 
 
 async def main():
-    async with websockets.serve(echo, "localhost", 8765):
+    async with websockets.serve(connection_handler, "localhost", 8765):
         await asyncio.Future()
 
 
