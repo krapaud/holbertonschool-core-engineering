bdbe7af434391830b0b09b97eb55ac483621e49b
krapaud <mur.mickael@gmail.com>
2026-07-02 11:47:16 +0200

    Rename handler to connection_handler and explicitly handle ConnectionClosed in unicast server

--- python_advanced/websockets/unicast_server.py
+++ python_advanced/websockets/unicast_server.py
@@ -2,21 +2,24 @@
 """WebSocket server with unicast message delivery."""
 import asyncio
 import websockets
+from websockets.exceptions import ConnectionClosed
 
 CLIENTS = set()
 
 
-async def handler(websocket):
+async def connection_handler(websocket):
     CLIENTS.add(websocket)
     try:
         async for message in websocket:
             await websocket.send(f"U:{message}")
+    except ConnectionClosed:
+        pass
     finally:
         CLIENTS.remove(websocket)
 
 
 async def main():
-    async with websockets.serve(handler, "localhost", 8765):
+    async with websockets.serve(connection_handler, "localhost", 8765):
         await asyncio.Future()
 
 
